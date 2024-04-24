dce800702056e9af9474d0965af2216768772188
Fazel Gheibi <fazelgheibi2001@gmail.com>
2024-04-24 17:50:14 +0330

    init project

--- src/test/java/com/example/log_collect/CommonTestData.java
+++ src/test/java/com/example/log_collect/CommonTestData.java
@@ -3,8 +3,6 @@ package com.example.log_collect;
 import com.example.log_collect.dtos.LoginDTO;
 import com.example.log_collect.models.enums.LogType;
 
-import java.util.UUID;
-
 public class CommonTestData {
     public static final String DEFAULT_STRING = "AAAAAAAAAAAAAAA";
     public static final String UPDATED_STRING = "BBBBBBBBBBBBBBB";
@@ -14,8 +12,7 @@ public class CommonTestData {
 
     public static final LogType DEFAULT_TYPE = LogType.INFO;
     public static final LogType UPDATED_TYPE = LogType.WARNING;
-
-
+    
     public static LoginDTO loginDTO() {
         LoginDTO loginDTO = new LoginDTO();
         loginDTO.setPassword(DEFAULT_PASSWORD);
